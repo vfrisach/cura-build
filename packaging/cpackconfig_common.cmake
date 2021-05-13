ccf7feaf4562dcd045cd94e897589db651db4e31
Víctor Frisach Boj <victor.frisach@apser.es>
2021-05-13 09:15:19 +0200

    cambio referencias

--- packaging/cpackconfig_common.cmake
+++ packaging/cpackconfig_common.cmake
@@ -1,13 +1,13 @@
 if(CURA_BUILDTYPE STREQUAL "")
     cpack_add_component(_cura
-                DISPLAY_NAME "Ultimaker Cura"
-                Description "Ultimaker Cura Executable and Data Files"
+                DISPLAY_NAME "Dynamical 3D Slicer"
+                Description "Dynamical 3D Slicer Executable and Data Files"
                 REQUIRED
     )
 else()
     cpack_add_component(_cura
-                DISPLAY_NAME "Ultimaker Cura ${CURA_BUILDTYPE}"
-                Description "Ultimaker Cura ${CURA_BUILDTYPE} Executable and Data Files"
+                DISPLAY_NAME "Dynamical 3D Slicer ${CURA_BUILDTYPE}"
+                Description "Dynamical 3D Slicer ${CURA_BUILDTYPE} Executable and Data Files"
                 REQUIRED
     )
 endif()
@@ -17,9 +17,9 @@ endif()
 # ========================================
 
 if(CURA_BUILDTYPE STREQUAL "")
-    set(CPACK_PACKAGE_NAME "Ultimaker Cura")
+    set(CPACK_PACKAGE_NAME "Dynamical 3D Slicer")
 else()
-    set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_BUILDTYPE}")
+    set(CPACK_PACKAGE_NAME "Dynamical 3D Slicer ${CURA_BUILDTYPE}")
 endif()
 string(REPLACE " " "_" CPACK_FILE_NAME_NO_SPACES "${CPACK_PACKAGE_NAME}")
 
@@ -37,25 +37,25 @@ set(CPACK_PACKAGE_VERSION_PATCH ${CURA_VERSION_PATCH})
 # betweem build types
 set(CURA_FULL_VERSION "${CURA_VERSION_MAJOR}.${CURA_VERSION_MINOR}.${CURA_VERSION_PATCH}")
 if(CURA_BUILDTYPE STREQUAL "")
-    set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_FULL_VERSION}")
+    set(CPACK_PACKAGE_NAME "Dynamical 3D Slicer ${CURA_FULL_VERSION}")
 else()
-    set(CPACK_PACKAGE_NAME "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
+    set(CPACK_PACKAGE_NAME "Dynamical 3D Slicer ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
 endif()
 
 set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/packaging/cura.ico")
-set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Ultimaker Cura - 3D Printing Software")
-set(CPACK_PACKAGE_CONTACT "Ruben Dulek <r.dulek@ultimaker.com>")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Dynamical 3D Slicer - 3D Printing Software")
+set(CPACK_PACKAGE_CONTACT "Victor Frisach <victor.frisach@apser.es>")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/packaging/cura_license.txt")
 
 # Differentiate between a normal Cura installation and that of a different build type
 if(CURA_BUILDTYPE STREQUAL "")
-    set(CPACK_CREATE_DESKTOP_LINKS Cura "Ultimaker Cura ${CURA_FULL_VERSION}")
-    set(CPACK_PACKAGE_EXECUTABLES Cura "Ultimaker Cura ${CURA_FULL_VERSION}")
-    set(CPACK_PACKAGE_INSTALL_DIRECTORY "Ultimaker Cura ${CURA_FULL_VERSION}")
+    set(CPACK_CREATE_DESKTOP_LINKS Cura "Dynamical 3D Slicer ${CURA_FULL_VERSION}")
+    set(CPACK_PACKAGE_EXECUTABLES Cura "Dynamical 3D Slicer ${CURA_FULL_VERSION}")
+    set(CPACK_PACKAGE_INSTALL_DIRECTORY "Dynamical 3D Slicer ${CURA_FULL_VERSION}")
 else()
-    set(CPACK_CREATE_DESKTOP_LINKS Cura "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
-    set(CPACK_PACKAGE_EXECUTABLES Cura "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
-    set(CPACK_PACKAGE_INSTALL_DIRECTORY "Ultimaker Cura ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
+    set(CPACK_CREATE_DESKTOP_LINKS Cura "Dynamical 3D Slicer ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
+    set(CPACK_PACKAGE_EXECUTABLES Cura "Dynamical 3D Slicer ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
+    set(CPACK_PACKAGE_INSTALL_DIRECTORY "Dynamical 3D Slicer ${CURA_BUILDTYPE} ${CURA_FULL_VERSION}")
 endif()
 
 # Forcing the CPack System Name to be either win32 or win64 for consistency purposes
